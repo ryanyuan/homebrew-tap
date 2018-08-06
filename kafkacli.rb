7f5a6deb9eb334bbf1df4f58a7a713ef0018fba5
Ryan Yuan <ryan.yuan@outlook.com>
2018-08-06 16:35:31 +1000

    Updated kafkacli to 0.0.6

--- kafkacli.rb
+++ kafkacli.rb
@@ -2,8 +2,8 @@ class Kafkacli < Formula
   include Language::Python::Virtualenv
     desc "Apache Kafka CLI utility tool."
     homepage "https://github.com/ryanyuan/kafkacli"
-    url "https://github.com/ryanyuan/kafkacli/releases/download/0.0.5/kafkacli-0.0.5.tar.gz"
-    sha256 "9a3379c9e61d5a592032dbd19c8151cf11f525a823e5ddd0515d29d10a1a6b08"
+    url "https://github.com/ryanyuan/kafkacli/releases/download/0.0.6/kafkacli-0.0.6.tar.gz"
+    sha256 "8b56589423325fe9bc208ee8d4578f8751a2cc7b42293d3c25d95e7ce24f2992"
     
     # depends_on "cmake" => :build
     depends_on "python@2"
