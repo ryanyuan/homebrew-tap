0e6b907cd6acdaaaaabc79e34ebae432a0ab595b
Ryan Yuan <ryan.yuan@outlook.com>
2018-08-06 14:17:19 +1000

    Updated kafkacli.rb

--- kafkacli.rb
+++ kafkacli.rb
@@ -2,7 +2,7 @@ class Kafkacli < Formula
     desc "Apache Kafka CLI utility tool."
     homepage "https://github.com/ryanyuan/kafkacli"
     url "https://github.com/ryanyuan/kafkacli/releases/download/0.0.4/kafkacli-0.0.4.zip"
-    sha256 "168562fe1783935b07eb2793e142879ec7e4b295deaa223e1f3afb1d3217a1bc"
+    sha256 "2d8c6bd50132181b544580c8d37b2d84727166decc47be18f34f5be0e00464c6"
     
     # depends_on "cmake" => :build
     depends_on "python@2"
@@ -10,12 +10,8 @@ class Kafkacli < Formula
     def install
       # ENV.deparallelize  # if your formula fails when building in parallel
       # copies all files to bin so we can execute the files from other scripts
-      bin.install '__init__.py'
-      bin.install '__main__.py'
-      bin.install 'util.py'
-      (bin + '__init__.py').chmod 0755
-      (bin + '__main__.py').chmod 0755
-      (bin + 'util.py').chmod 0755
+      bin.install 'kafkacli'
+      (bin + 'kafkacli').chmod 0755
     end
   end
   
